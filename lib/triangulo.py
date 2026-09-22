3a4a6c439d8f8f1e4da4111cece0d6a7906c534a
MarioMartinezGonzalez <mmartinezg2025@gmail.com>
2026-09-22 17:57:51 +0200

    Creadas las funciones triángulo

---
+++ lib/triangulo.py
@@ -0,0 +1,8 @@
+def get_identificador():
+    return "triángulo"
+
+def get_area(base, altura):
+    return 0.5*base*altura
+
+def get_perimetro(lado_a, lado_b, lado_c):
+    return lado_a + lado_b + lado_c
