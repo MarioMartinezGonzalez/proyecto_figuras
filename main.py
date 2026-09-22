8b694b4df745848833d4bad98fac5ae3b302edef
MarioMartinezGonzalez <mmartinezg2025@gmail.com>
2026-09-22 18:48:17 +0200

    Añadidas funciones del triangulo al main

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ print(cuadrado.get_identificador())
 lado = 4
 print(f'El area del cuadrado de lado {lado} es: {cuadrado.get_area(lado)} y el perímetro es: {cuadrado.get_perimetro(lado)}')
 
+
+base= 4
+altura=2
+print(triangulo.get_identificador())
+print(f'El área de un {triangulo.get_identificador()} de base {base}, y de altura {altura} es: {triangulo.get_area(base,altura)} y el perímetro es :{triangulo.get_perimetro(base,base,base)}')
